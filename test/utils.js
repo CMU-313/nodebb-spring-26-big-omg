33aac420eaef6b92453dbf53749959bdab664c38
josegerm <josegerm@andrew.cmu.edu>
2026-03-24 16:08:25 +0000

    added fast-check and fixed tests

--- test/utils.js
+++ test/utils.js
@@ -263,20 +263,25 @@ describe('Utility Methods', () => {
 				   
 					const merged = utils.merge(cloneA, cloneB);
 				   
-					const equal = (a, b) => (a === b || (Number.isNaN(a) && Number.isNaN(b)));
 					// 1. All keys from B must be in the merged object with B's values
 					for (const key of Object.keys(objB)) {
-						if (!equal(merged[key], objB[key])) return false;
+						if (merged[key] !== objB[key]) return false;
 					}
-					   
+				   
 					// 2. All keys from A (that aren't in B) must retain A's values
 					for (const key of Object.keys(objA)) {
-						if (!(key in objB) && !equal(merged[key], objA[key])) return false;
+						if (!(key in objB) && merged[key] !== objA[key]) return false;
 					}
+				   
 					return true;
 				}
 			)
 		);
+	});
+
+	it('should return the file extesion', (done) => {
+		assert.equal(utils.fileExtension('/path/to/some/file.png'), 'png');
+		done();
 	});
 
 	it('should return file mime type', (done) => {
@@ -453,13 +458,13 @@ describe('Utility Methods', () => {
 		it('should correctly parse stringified JSON objects', () => {
 			fc.assert(
 				fc.property(
-					fc.jsonValue(),
-					(val) => {
-						const stringified = JSON.stringify(val);
+					fc.jsonObject(),
+					(obj) => {
+						const stringified = JSON.stringify(obj);
 						const result = utils.toType(stringified);
 					   
 						// Use deepStrictEqual because we are comparing objects
-						assert.deepStrictEqual(result, JSON.parse(stringified));
+						assert.deepStrictEqual(result, obj);
 						return true;
 					}
 				)
@@ -564,7 +569,7 @@ describe('Utility Methods', () => {
 			fc.property(
 				fc.string(),
 				// Generate a string consisting only of whitespace characters
-				fc.string({ unit: fc.constantFrom(' ', '\t', '\n', '\r') }),
+				fc.stringOf(fc.constantFrom(' ', '\t', '\n', '\r')),
 				(randomStr, randomWhitespace) => {
 					// E.g., if randomStr is "hello  ", rtrim("hello  " + " \t") should equal rtrim("hello  ")
 					const combined = randomStr + randomWhitespace;
